52a7bc3c3c0dbe381326acef18166ba3fe57bfc1
MajorityAttack <majorityattack@gmail.com>
2019-01-18 19:36:38 +0800

    Toast

---
+++ App.js
@@ -0,0 +1,57 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+
+import {NativeModules} from 'react-native';
+var HelloWorld = NativeModules.HelloWorld;
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+
+  // async function to call the Java native method
+  async sayHiFromJava() {
+    HelloWorld.sayHi( (err) => {console.log(err)}, (msg) => {console.log(msg)} );
+  }
+  
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Invoke native Java code</Text>
+        <Text onPress={()=>HelloWorld.show('Awesome',HelloWorld.LONG)}>Click</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
---
+++ android/app/src/main/java/com/myproject/HelloWorldModule.java
@@ -0,0 +1,48 @@
+package com.myproject;
+
+import android.widget.Toast;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.uimanager.IllegalViewOperationException;
+
+import java.util.Map;
+import java.util.HashMap;
+ 
+public class HelloWorldModule extends ReactContextBaseJavaModule {
+ 
+    public HelloWorldModule(ReactApplicationContext reactContext) {
+        super(reactContext); //required by React Native
+    }
+ 
+    @Override
+    //getName is required to define the name of the module represented in JavaScript
+    public String getName() { 
+        return "HelloWorld";
+    }
+
+    @Override
+    public Map<String, Object> getConstants() {
+      final Map<String, Object> constants = new HashMap<>();
+      constants.put("SHORT", Toast.LENGTH_SHORT);
+      constants.put("LONG", Toast.LENGTH_LONG);
+      return constants;
+    }
+ 
+    @ReactMethod
+    public void sayHi(Callback errorCallback, Callback successCallback) {
+        try {
+            System.out.println("Greetings from Java");
+            successCallback.invoke("Callback : Greetings from Java");
+        } catch (IllegalViewOperationException e) {
+            errorCallback.invoke(e.getMessage());
+        }
+    }
+
+    @ReactMethod
+    public void show(String message, int duration) {
+      Toast.makeText(getReactApplicationContext(), message, duration).show();
+    }
+}
